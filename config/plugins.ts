7ac8cd49ff421961a81c2c4b9e35977699333799
avas27027 <alvas27027@gmail.com>
2023-05-10 20:33:04 -0500

    storage corr

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,24 +1,25 @@
+import * as data from "../strapi-test-b1d0d-firebase-adminsdk-238fk-b7674cea58.json";
 module.exports = ({ env }) => ({
     meilisearch: {
-      config: {
-        // Your meili host
-        host: "https://ms-dbbe741936fb-3515.sfo.meilisearch.io",
-        // Your master key or private key
-        apiKey: "4bf239e1614ad7c802e75d5b4ade24678c49cfe5",
-      }
+        config: {
+            // Your meili host
+            host: "https://ms-dbbe741936fb-3515.sfo.meilisearch.io",
+            // Your master key or private key
+            apiKey: "4bf239e1614ad7c802e75d5b4ade24678c49cfe5",
+        }
     },
     // ...
     upload: {
-      config: {
-        provider: "strapi-provider-firebase-storage",
-        providerOptions: {
-          serviceAccount: require("../strapi-test-b1d0d-firebase-adminsdk-238fk-b7674cea58.json"),
-          // Custom bucket name
-          bucket: env("STORAGE_BUCKET_URL"),
-          sortInStorage: true, // true | false
-          debug: false, // true | false
+        config: {
+            provider: "strapi-provider-firebase-storage",
+            providerOptions: {
+                serviceAccount: data,
+                // Custom bucket name
+                bucket: env("STORAGE_BUCKET_URL"),
+                sortInStorage: true, // true | false
+                debug: false, // true | false
+            },
         },
-      },
     },
     // ...
-  });
+});
