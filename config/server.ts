0783aacc1fa607242ecc1360294723f91ea8de72
avas27027 <alvas27027@gmail.com>
2023-05-18 23:46:21 -0500

    del env

--- config/server.ts
+++ config/server.ts
@@ -2,8 +2,8 @@ export default ({ env }) => ({
   //host: "localhost",
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  //url: env('URL', 'localhost'),
-  url: env('', 'http://localhost:1337'),
+  url: env('URL', 'localhost'),
+  //url: env('', 'http://localhost:1337'),
   proxy: true,
   app: {
     keys: env.array('APP_KEYS'),
